6685a5f9b4695e5462fc078f14a96d96763a7be7
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2022-10-05 15:20:53 +0300

    components

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,46 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-
+import Todolist from './components/Todolist';
+import Todotable from './components/Todotable';
 
 function App() {
-  const [todo, setTodo] = useState({desc: '', date: ''})
-  const [todos, setTodos] = useState([])
-
-  const inputChanged = (event) => {
-    setTodo({...todo, [event.target.name]: event.target.value});
-  }
-
-  const addTodo = (event) => {
-    event.preventDefault();
-    setTodos([...todos, todo]);
-  }
-
-  const deleteItem = (index) => () => {
-    setTodos((todos) => todos.filter((_, i) => i !== index))
-  }
   
   return (
     <div className="App">
       <p>Add todo:</p>
-     <form onSubmit={addTodo}>
-      <label>Date:</label>
-      <input type="date" name="date" value={todo.date} onChange={inputChanged}/>
-      <label>Description:</label>
-      <input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
-      <input type="submit" value="Add"/>
-     </form>
-     <table>
-                <tbody>
-                    <tr><th>Date</th><th>Description</th></tr>
-                    {todos.map((todo, index) =>
-                    <tr key={todo.desc}>
-                    <td>{todo.date}</td>
-                    <td>{todo.desc}</td>
-                    <td><button onClick={deleteItem(index)}>Delete</button></td>
-                    </tr>
-                    )}
-                </tbody>
-            </table>
+      <Todolist />
+      <Todotable />
     </div>
   );
 }
